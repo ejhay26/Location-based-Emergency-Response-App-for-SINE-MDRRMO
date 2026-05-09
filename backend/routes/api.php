8fab68917c285198ed760e7f5954abde0f35f388
ejhay26 <ejperez623@gmail.com>
2026-05-09 20:38:58 +0800

    feat: add route for updating medical profile in AuthController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,6 +16,7 @@ Route::post('/create-dispatcher', [AuthController::class, 'createDispatcher']);
 Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
 Route::post('/update-profile-picture', [AuthController::class, 'updateProfilePicture']);
 Route::post('/update-password', [AuthController::class, 'updatePassword']);
+Route::post('/update-medical-profile', [AuthController::class, 'updateMedicalProfile']); // NEW: Medical Profile
 
 // Emergency & Dispatch Routes
 Route::post('/submit-sos', [EmergencyController::class, 'submitSos']);
@@ -28,7 +29,7 @@ Route::post('/resolve-emergency', [EmergencyController::class, 'resolveEmergency
 Route::get('/archived-emergencies', [EmergencyController::class, 'getArchivedEmergencies']);
 Route::get('/analytics', [EmergencyController::class, 'getAnalytics']);
 
-// NEW: Hazard & Broadcast Routes
+// Hazard & Broadcast Routes
 Route::post('/submit-hazard', [EmergencyController::class, 'submitHazard']);
 Route::get('/active-hazards', [EmergencyController::class, 'getActiveHazards']);
 Route::post('/create-broadcast', [EmergencyController::class, 'createBroadcast']);
