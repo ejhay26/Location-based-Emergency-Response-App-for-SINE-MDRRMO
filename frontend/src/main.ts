fb3bd632891e7319a1fcc0eb064dbcb03b55788d
ejhay26 <ejperez623@gmail.com>
2026-06-24 12:47:31 +0800

    feat: integrate HTTP interceptors and configure Ionic Angular mode in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+import { ErrorInterceptorService } from './app/services/error-interceptor';
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
+    // Pass your config object directly inside provideIonicAngular!
+    provideIonicAngular({
+      mode: 'md'
+    }),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptorService, multi: true },
   ],
 });
