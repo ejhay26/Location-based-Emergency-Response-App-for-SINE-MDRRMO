3d48b8efa32f7cd60b6a4385ade9118a806f0bdc
ejhay26 <ejperez623@gmail.com>
2026-06-28 17:30:49 +0800

    feat: update app name in capacitor configuration

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'ph.gov.sine.mdrrmo.emergencyresponse',
-  appName: 'frontend',
+  appName: 'MDRRMO SINE Emergency Response App',
   webDir: 'www'
 };
 
