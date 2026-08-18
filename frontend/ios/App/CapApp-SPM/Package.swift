4fb2838a41bc37132a6b083c754e56d5d5ce6741
Emmanuel John Perez <ejperez623@gmail.com>
2026-08-19 00:27:50 +0800

    feat: add custom URL scheme for deep linking and include Capawesome SMS Retriever dependency

--- frontend/ios/App/CapApp-SPM/Package.swift
+++ frontend/ios/App/CapApp-SPM/Package.swift
@@ -18,7 +18,8 @@ let package = Package(
         .package(name: "CapacitorHaptics", path: "..\..\..\node_modules\@capacitor\haptics"),
         .package(name: "CapacitorKeyboard", path: "..\..\..\node_modules\@capacitor\keyboard"),
         .package(name: "CapacitorPushNotifications", path: "..\..\..\node_modules\@capacitor\push-notifications"),
-        .package(name: "CapacitorStatusBar", path: "..\..\..\node_modules\@capacitor\status-bar")
+        .package(name: "CapacitorStatusBar", path: "..\..\..\node_modules\@capacitor\status-bar"),
+        .package(name: "CapawesomeCapacitorAndroidSmsRetriever", path: "..\..\..\node_modules\@capawesome\capacitor-android-sms-retriever")
     ],
     targets: [
         .target(
@@ -32,7 +33,8 @@ let package = Package(
                 .product(name: "CapacitorHaptics", package: "CapacitorHaptics"),
                 .product(name: "CapacitorKeyboard", package: "CapacitorKeyboard"),
                 .product(name: "CapacitorPushNotifications", package: "CapacitorPushNotifications"),
-                .product(name: "CapacitorStatusBar", package: "CapacitorStatusBar")
+                .product(name: "CapacitorStatusBar", package: "CapacitorStatusBar"),
+                .product(name: "CapawesomeCapacitorAndroidSmsRetriever", package: "CapawesomeCapacitorAndroidSmsRetriever")
             ]
         )
     ]
